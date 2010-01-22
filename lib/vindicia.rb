7add0c3daaf8815cc35ceca4c29c229b7c537fbc
Jamie Macey <jamie@almlabs.com>
2010-01-22 17:03:43 -0500

    BillingPlan integration spec, refactor accessors

--- lib/vindicia.rb
+++ lib/vindicia.rb
@@ -34,40 +34,36 @@ module Vindicia
     end
   end
   
-  class SoapClient
-    attr_reader :wsdl_, :soap
-    
-    def initialize(test=true)
-      @class = self.class.to_s.split('::').last
-      @login = Vindicia.login
-      @password = Vindicia.password
-    
-      @soap = wsdl.create_rpc_driver
-      # prodtest/staging wsdl are identical to production, so force a correct url
-      @soap.proxy.instance_variable_set('@endpoint_url', Vindicia.endpoint)
-      @soap.wiredump_dev = STDERR if $DEBUG
+  module SoapClient
+    def soap
+      @soap ||= begin
+        s = wsdl.create_rpc_driver
+        # prodtest/staging wsdl are identical to production, so force a correct url
+        s.proxy.instance_variable_set('@endpoint_url', Vindicia.endpoint)
+        s.wiredump_dev = STDERR if $DEBUG
+        s
+      end
     end
 
     def method_missing(method, *args)
       if args[0].kind_of? Hash
-        args[0] = self.class.defaults_for(method).merge(args.first)
+        args[0] = defaults_for(method).merge(args.first)
       end
       with_soap do |soap|
-        soap.send(method, Vindicia.auth, *args)
+        soap.send(method, Vindicia.auth, *args.clone)
       end
     end
     
-    def self.default(method, defaults)
+    def default(method, defaults)
       @defaults ||= {}
       @defaults[method] = defaults
     end
     
-    def self.defaults_for(method)
+    def defaults_for(method)
       @defaults ||= {}
       @defaults[method] || {}
     end
 
-  private
     def quietly
       # Squelch warnings on stderr
       stderr = $stderr
@@ -78,32 +74,48 @@ module Vindicia
     end
   
     def with_soap
-      ret = quietly { yield @soap }
-      @soap.reset_stream
+      ret = quietly { yield soap }
+      soap.reset_stream
       ret
     end
   
     def wsdl
-      @@wsdl_ ||= quietly do
-        SOAP::WSDLDriverFactory.new(Vindicia.wsdl(@class))
+      @wsdl ||= quietly do
+        SOAP::WSDLDriverFactory.new(Vindicia.wsdl(self.to_s.split('::').last))
       end
     end
   end
   
-  class Account < SoapClient
+  class Account
+    extend SoapClient
     default :update, :emailTypePreference => 'plaintext'
   end
   
-  class Activity < SoapClient ; end
-  class Address < SoapClient ; end
-  class AutoBill < SoapClient ; end
-  class BillingPlan < SoapClient ; end
-  class Chargeback < SoapClient ; end
-  class Entitlement < SoapClient ; end
-  class PaymentMethod < SoapClient ; end
-  class PaymentProvider < SoapClient ; end
-  class Product < SoapClient ; end
-  class Refund < SoapClient ; end
-  class Transaction < SoapClient ; end
+  # class Activity
+  #   extend SoapClient
+  # end
+  # 
+  # class Address
+  #   extend SoapClient
+  # end
+
+  class AutoBill
+    extend SoapClient
+  end
+
+  class BillingPlan
+    extend SoapClient
+  end
+
+  # class Chargeback < SoapClient ; end
+  # class Entitlement < SoapClient ; end
+  # class PaymentMethod < SoapClient ; end
+  # class PaymentProvider < SoapClient ; end
+  class Product
+    extend SoapClient
+  end
+
+  # class Refund < SoapClient ; end
+  # class Transaction < SoapClient ; end
   
 end
--- spec/vindicia_spec.rb
+++ spec/vindicia_spec.rb
@@ -5,13 +5,13 @@ require 'spec/authenticate'
 
 describe Vindicia::Account do
   before :each do
-    @client = Vindicia::Account.new
+    @client = Vindicia::Account
   end
 
   it 'should return a singleton soap wrapper' do
-    a = Vindicia::Account.new
-    b = Vindicia::Account.new
-    a.wsdl_.should be_equal(b.wsdl_)
+    a = Vindicia::Account
+    b = Vindicia::Account
+    a.wsdl.should be_equal(b.wsdl)
   end
   
   describe '#update' do
@@ -50,3 +50,46 @@ describe Vindicia::Account do
   end
   
 end
+
+describe Vindicia do
+  it 'should set up a recurring purchase' do
+    # Product, BillingPlan are set up in CashBox by hand
+    ret, account, created = Vindicia::Account.update({
+      :merchantAccountId => Time.now.to_i.to_s,
+      :name => 'Integration User'
+    })
+    ret, account, validated = Vindicia::Account.updatePaymentMethod({
+      # Account
+      :VID => account['VID'],
+      :emailTypePreference => 'plaintext'
+    }, {
+      # Payment Method
+      :type => 'CreditCard',
+      :creditCard => {
+        :account => '4783684405207461',
+        :expirationDate => '201207',
+        :hashType => 'sha1'
+      },
+      :accountHolderName => 'John Smith',
+      :billingAddress => {
+        :name => 'John Smith',
+        :addr1 => '123 Main St',
+        :city => 'Toronto',
+        :district => 'Ontario',
+        :country => 'Canada',
+        :postalCode => 'M4V 5X7'
+      },
+      :merchantPaymentMethodId => "Purchase.id #{Time.now.to_i}"
+    }, true, 'Update', nil)
+    ret, product = Vindicia::Product.fetchByMerchantProductId('em-2-PREMIUM-USD')
+    ret, billing = Vindicia::BillingPlan.fetchByMerchantBillingPlanId('em-2-PREMIUM-USD')
+    ret, autobill, created, authstatus, firstBillDate, firstBillAmount, firstBillingCurrency =
+      Vindicia::AutoBill.update({
+        :account => {:VID => account['VID'], :emailTypePreference => 'plaintext'},
+        :product => {:VID => product['VID'], :status => product['status'], :taxClassification => product['taxClassification']},
+        :billingPlan => {:VID => billing['VID'], :status => billing['status']},
+        :status => 'Active'
+      }, 'Fail', true, 100)
+  end
+end
+
