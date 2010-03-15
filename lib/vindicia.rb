1457caee08125c1d718b1ddda66e2bee91bc5f6b
Jamie Macey <jamie@almlabs.com>
2010-03-15 14:00:46 -0400

    stub remaining data types

--- lib/vindicia.rb
+++ lib/vindicia.rb
@@ -183,57 +183,22 @@ module Vindicia
   end
   
   # API classes
-  class Account < SoapObject
-    extend SoapClient
-    
+  class Account           < SoapObject; extend SoapClient
     default :updatePaymentMethod, {}, {}, true, 'Update', nil
   end
-  
-  class Activity < SoapObject
-    extend SoapClient
-  end
-
-  class Address < SoapObject
-    extend SoapClient
-  end
-
-  class AutoBill < SoapObject
-    extend SoapClient
-    
+  class Activity          < SoapObject; extend SoapClient end
+  class Address           < SoapObject; extend SoapClient end
+  class AutoBill          < SoapObject; extend SoapClient
     default :update, {}, 'Fail', true, 100
   end
-
-  class BillingPlan < SoapObject
-    extend SoapClient
-  end
-
-  class Chargeback < SoapObject
-    extend SoapClient
-  end
-
-  class Entitlement < SoapObject
-    extend SoapClient
-  end
-
-  class PaymentMethod < SoapObject
-    extend SoapClient
-  end
-
-  class PaymentProvider < SoapObject
-    extend SoapClient
-  end
-
-  class Product < SoapObject
-    extend SoapClient
-  end
-  
-  class Refund < SoapObject
-    extend SoapClient
-  end
-
-  class Transaction < SoapObject
-    extend SoapClient
-
+  class BillingPlan       < SoapObject; extend SoapClient end
+  class Chargeback        < SoapObject; extend SoapClient end
+  class Entitlement       < SoapObject; extend SoapClient end
+  class PaymentMethod     < SoapObject; extend SoapClient end
+  class PaymentProvider   < SoapObject; extend SoapClient end
+  class Product           < SoapObject; extend SoapClient end
+  class Refund            < SoapObject; extend SoapClient end
+  class Transaction       < SoapObject; extend SoapClient
     default :auth, {}, 100, false
     default :authCapture, {}, false
   end
@@ -244,10 +209,47 @@ module Vindicia
     def response; self.returnString; end
   end
 
-  # Stub data classes
-  class CaptureResult     < SoapObject ; end
-  class CreditCard        < SoapObject ; end
-  class TransactionStatus < SoapObject ; end
+  # Stub data types
+  class ActivityCancellation          < SoapObject ; end
+  class ActivityEmailContact          < SoapObject ; end
+  class ActivityFulfillment           < SoapObject ; end
+  class ActivityLogin                 < SoapObject ; end
+  class ActivityLogout                < SoapObject ; end
+  class ActivityNamedValue            < SoapObject ; end
+  class ActivityNote                  < SoapObject ; end
+  class ActivityPhoneContact          < SoapObject ; end
+  class ActivityTypeArg               < SoapObject ; end
+  class ActivityURIView               < SoapObject ; end
+  class ActivityUsage                 < SoapObject ; end
+  class Authentication                < SoapObject ; end
+  class BillingPlanPeriod             < SoapObject ; end
+  class BillingPlanPrice              < SoapObject ; end
+  class Boleto                        < SoapObject ; end
+  class CancelResult                  < SoapObject ; end
+  class CaptureResult                 < SoapObject ; end
+  class CreditCard                    < SoapObject ; end
+  class DirectDebit                   < SoapObject ; end
+  class ECP                           < SoapObject ; end
+  class ElectronicSignature           < SoapObject ; end
+  class EmailTemplate                 < SoapObject ; end
+  class MerchantEntitlementId         < SoapObject ; end
+  class MetricStatistics              < SoapObject ; end
+  class NameValuePair                 < SoapObject ; end
+  class PayPal                        < SoapObject ; end
+  class SalesTax                      < SoapObject ; end
+  class ScoreCode                     < SoapObject ; end
+  class TaxExemption                  < SoapObject ; end
+  class Token                         < SoapObject ; end
+  class TokenAmount                   < SoapObject ; end
+  class TokenTransaction              < SoapObject ; end
+  class TransactionItem               < SoapObject ; end
+  class TransactionStatus             < SoapObject ; end
+  class TransactionStatusBoleto       < SoapObject ; end
+  class TransactionStatusCreditCard   < SoapObject ; end
+  class TransactionStatusDirectDebit  < SoapObject ; end
+  class TransactionStatusECP          < SoapObject ; end
+  class TransactionStatusPayPal       < SoapObject ; end
+  class WebSession                    < SoapObject ; end
 end
 
 class WSDL::XMLSchema::SimpleRestriction
